2dc71eac8cad196209f343fc5d8d34c29ed7da57
DAVID HUARICANCHA <davidjss.dev@gmail.com>
2022-10-11 12:58:59 -0500

    add

---
+++ src/pages/MainHome.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Home, About, Projects, Experience, Contact } from '../sections';
+
+const MainHome = () => {
+	return (
+		<div>
+			<Home />
+			<About />
+			<Projects />
+			<Experience />
+			<Contact />
+		</div>
+	);
+};
+
+export default MainHome;
