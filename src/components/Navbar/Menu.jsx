c681acace9bffff5f23c29e0416d66f81daf2bc9
David Jesus <62025422+davidjss04@users.noreply.github.com>
2022-10-22 18:01:55 -0500

    okiii

--- src/components/Navbar/Menu.jsx
+++ src/components/Navbar/Menu.jsx
@@ -1,33 +1,46 @@
 import React, { useState } from 'react';
 import {
+	Box,
 	Drawer,
 	IconButton,
 	List,
 	ListItemButton,
 	ListItemIcon,
 	ListItemText,
+	useTheme,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import styled from '@emotion/styled';
+
 const pages = ['ABOUT', 'EXPERIENCE', 'PROJECTS', 'CONTACT'];
 
 const Menu = () => {
 	const [openMenu, setOpenMenu] = useState(false);
+	const theme = useTheme();
+
 	return (
 		<React.Fragment>
 			<Drawer
 				anchor="left"
 				open={openMenu}
 				onClose={() => setOpenMenu(false)}
+				sx={{
+					'& .MuiDrawer-paper': {
+						backgroundColor: theme.palette.background.default,
+					},
+				}}
 			>
-				<List>
-					{pages.map((page, index) => (
-						<ListItemButton key={index}>
-							<ListItemIcon>
-								<ListItemText>{page}</ListItemText>
-							</ListItemIcon>
-						</ListItemButton>
-					))}
-				</List>
+				<Box component="div" sx={{ width: 250 }} role="presentation">
+					<StyledList>
+						{pages.map((page, index) => (
+							<StyledListItem key={index}>
+								<ListItemIcon>
+									<ListItemText>{page}</ListItemText>
+								</ListItemIcon>
+							</StyledListItem>
+						))}
+					</StyledList>
+				</Box>
 			</Drawer>
 			<IconButton
 				sx={{ color: 'white', marginLeft: 'auto' }}
@@ -38,5 +51,19 @@ const Menu = () => {
 		</React.Fragment>
 	);
 };
+
+const StyledList = styled(List)(({ theme }) => ({
+	width: 'auto',
+	marginTop: theme.spacing(4),
+}));
+
+const StyledListItem = styled(ListItemButton)(({ theme }) => ({
+	display: 'flex',
+	justifyContent: 'center',
+	padding: theme.spacing(2, 0),
+	'&:hover': {
+		backgroundColor: 'rgb(80,80,80)',
+	},
+}));
 
 export default Menu;
